5df8015ec33f29c0804626a0e13c27b73b98e5eb
Bisma8090 <bismaarshad8090@gmail.com>
2026-03-11 08:41:09 +0500

    fix vercel serverless deployment

--- v2-auth-mongodb/server.js
+++ v2-auth-mongodb/server.js
@@ -27,8 +27,4 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 // Error handling
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
- console.log(`Server running on port ${PORT}`)
-})
+module.exports = app
