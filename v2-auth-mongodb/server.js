c437512778803044cc3e7143e75b41a5d031c68b
Bisma8090 <bismaarshad8090@gmail.com>
2026-03-11 10:13:04 +0500

    fixed swagger

--- v2-auth-mongodb/server.js
+++ v2-auth-mongodb/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const swaggerUI = require("swagger-ui-express");
 const swaggerSpec = require("./swagger");
+const swaggerUiDist = require("swagger-ui-dist").getAbsoluteFSPath(); // <-- add this
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const connectDB = require("./config/db");
@@ -19,8 +20,15 @@ app.use("/api", authRoutes);
 // Task routes
 app.use("/api", taskRoutes);
 
-// Swagger docs
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec, { explorer: true }));
+// ✅ Swagger docs
+
+// Serve static Swagger assets (JS/CSS)
+app.use("/api-docs", express.static(swaggerUiDist));
+
+// Serve Swagger HTML
+app.get("/api-docs", (req, res) => {
+  res.send(swaggerUI.generateHTML(swaggerSpec));
+});
 
 // Error handling
 app.use(errorHandler);
