b2772e83de62ce4820955d0ee678fd1e53ad8dd0
Bisma8090 <bismaarshad8090@gmail.com>
2026-03-13 01:06:19 +0500

    Backend fixed

--- v2-auth-mongodb/server.js
+++ v2-auth-mongodb/server.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://week03day03.vercel.app/"
+      "https://week03day03.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
